42d8ba4961c6bc97f1295ec3bc115f275279c41a
Ruchi Jian <learnwithfunroo@gmail.com>
2026-09-20 20:28:03 +0530

    created changes

--- Backend/src/services/dbuser.js
+++ Backend/src/services/dbuser.js
@@ -123,7 +123,7 @@ export const loginUser = async ({ email, password }) => {
 };
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
