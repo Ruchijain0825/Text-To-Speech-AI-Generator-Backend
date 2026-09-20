ebfa49de09cc2b4155d30af97dc656e85a99ce5c
Ruchi Jian <learnwithfunroo@gmail.com>
2026-09-20 20:22:28 +0530

    created changes

--- Backend/src/services/dbuser.js
+++ Backend/src/services/dbuser.js
@@ -122,13 +122,14 @@ export const loginUser = async ({ email, password }) => {
     };
 };
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:
-    {
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
-})
+});
 transporter.verify((error, success) => {
     if (error) {
         console.error("MAIL SERVER ERROR:", error);
